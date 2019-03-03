503186be3f134084c5fb50ce1200e0fe070d6fb5
Sergii Shymko <sergey@shymko.net>
2019-03-02 21:51:17 -0800

    Implement sticky session dispatcher
    - Dispatch requests to worker processes by Session ID
    - Recognize Session ID in query string
    - Recognize Session ID in cookies
    - Declare Composer package metadata
    - Document sticky session usage

---
+++ README.md
@@ -0,0 +1,89 @@
+Request Dispatch for Swoole
+===========================
+
+This is a collection of request [dispatch strategies](https://www.swoole.co.uk/docs/modules/swoole-server/configuration#dispatch_func) for [Swoole](https://www.swoole.co.uk/) web-server that compliment the built-in [dispatch modes](https://www.swoole.co.uk/docs/modules/swoole-server/configuration#dispatch_mode).
+Sticky session dispatch strategy also known as the session affinity is the cornerstone of this library.
+
+**Features:**
+- Sticky session
+    - Dispatch requests to workers by Session ID
+    - Session ID in query string
+    - Session ID in cookies
+
+## Installation
+
+The library is to be installed via [Composer](https://getcomposer.org/) as a dependency:
+```bash
+composer require upscale/swoole-dispatch
+```
+## Dispatch Strategies
+
+### Sticky Session
+
+Sticky session also known as session affinity dispatches request to worker processes by Session ID.
+It recognizes the Session ID passed in the query string and cookie headers in that order of priority.
+
+Anonymous requests are being dispatched as in the [fixed mode](https://www.swoole.co.uk/docs/modules/swoole-server/configuration#dispatch_mode).
+
+Register the sticky session dispatcher:
+```php
+require 'vendor/autoload.php';
+
+$dispatcher = new \Upscale\Swoole\Dispatch\StickySession(
+    session_name(),
+    ini_get('session.sid_length')
+);
+
+$server = new \Swoole\Http\Server('127.0.0.1', 8080);
+$server->set([
+    'worker_num'    => 4,
+    'dispatch_func' => $dispatcher,
+]);
+
+$server->on('request', function ($request, $response) {
+    $response->header('Content-Type', 'text/plain');
+    $response->end('Served by worker PID ' . getmypid() . PHP_EOL);
+});
+
+$server->start();
+```
+
+Send requests with and without the session context:
+- Anonymous requests distributed between all workers:
+    ```bash
+    curl -s 'http://127.0.0.1:8080/?[1-4]' -H 'Connection: close'
+    ```
+    ```
+    Served by worker PID 21601
+    Served by worker PID 21602
+    Served by worker PID 21603
+    Served by worker PID 21604
+    ```
+- Session ID in query string dispatched to a dedicated worker:
+    ```bash
+    curl -s 'http://127.0.0.1:8080/?PHPSESSID=ExampleSessionIdentifier11&[1-4]' -H 'Connection: close'
+    ```
+    ```
+    Served by worker PID 21603
+    Served by worker PID 21603
+    Served by worker PID 21603
+    Served by worker PID 21603
+    ```
+- Session ID in cookies dispatched to a dedicated worker:
+    ```bash
+    curl -s 'http://127.0.0.1:8080/?[1-4]' -H 'Cookie: PHPSESSID=ExampleSessionIdentifier22' -H 'Connection: close'
+    ```
+    ```
+    Served by worker PID 21604
+    Served by worker PID 21604
+    Served by worker PID 21604
+    Served by worker PID 21604
+    ```
+
+## Contributing
+
+Pull Requests with fixes and improvements are welcome!
+
+## License
+
+Licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
---
+++ src/StickySession.php
@@ -0,0 +1,98 @@
+<?php
+namespace Upscale\Swoole\Dispatch;
+
+/**
+ * Dispatch requests to workers according to session ID for sticky session aka session affinity.
+ * All requests belonging to a session will be dispatched to a dedicated worker process.
+ * Session ID is recognized in a query string and cookies in that order of priority.
+ *
+ * This strategy is complimentary to the session locking and can compensate for the lack of thereof.
+ * It prevents race conditions in workers competing for an exclusive lock of the same session ID.
+ * Workers only pick up requests of their respective sessions as well as anonymous requests.
+ */
+class StickySession
+{
+    /**#@+
+     * Connection dispatch type
+     *
+     * @link https://www.swoole.co.uk/docs/modules/swoole-server/configuration#dispatch_func
+     */
+    const CONNECTION_FETCH  = 10;
+    const CONNECTION_START  = 5;
+    const CONNECTION_CLOSE  = 4;
+    /**#@-*/
+
+    /**
+     * @var string
+     */
+    protected $sidMarker;
+
+    /**
+     * @var int
+     */
+    protected $sidMarkerLength;
+
+    /**
+     * @var int
+     */
+    protected $sidLength;
+
+    /**
+     * @var array
+     */
+    protected $dispatchMap = [];
+
+    /**
+     * Inject dependencies
+     *
+     * @param string $sessionName
+     * @param string $sessionIdLength
+     */
+    public function __construct($sessionName, $sessionIdLength)
+    {
+        $this->sidMarker = $sessionName . '=';
+        $this->sidMarkerLength = strlen($this->sidMarker);
+        $this->sidLength = $sessionIdLength;
+    }
+
+    /**
+     * Resolve requests to corresponding workers based on session ID
+     *
+     * @param \Swoole\Http\Server $server
+     * @param int $fd Client ID number
+     * @param int $type Dispatch type
+     * @param string $data Request packet data (0-8180 bytes)
+     * @return int Worker ID number
+     */
+    public function __invoke(\Swoole\Http\Server $server, $fd, $type, $data)
+    {
+        if (array_key_exists($fd, $this->dispatchMap)) {
+            $workerId = $this->dispatchMap[$fd];
+        } else {
+            $sessionId = $this->extractSessionId($data);
+            $requestId = $sessionId ? crc32($sessionId) : ($fd - 1);
+            $workerId = $requestId % $server->setting['worker_num'];
+            $this->dispatchMap[$fd] = $workerId;
+        }
+        if ($type == self::CONNECTION_CLOSE) {
+            unset($this->dispatchMap[$fd]);
+        }
+        return $workerId;
+    }
+
+    /**
+     * Extract session ID from an HTTP request message
+     *
+     * @param string $message
+     * @return string|null
+     */
+    protected function extractSessionId($message)
+    {
+        // Query string has priority over cookies as request line precedes headers  
+        $pos = strpos($message, $this->sidMarker);
+        if ($pos !== false) {
+            return substr($message, $pos + $this->sidMarkerLength, $this->sidLength);
+        }
+        return null;
+    }
+}
