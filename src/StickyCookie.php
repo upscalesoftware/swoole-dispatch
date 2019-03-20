8b13e95668a923255012741103c75f47815c4da6
Sergii Shymko <sergey@shymko.net>
2019-03-20 13:26:32 -0700

    Introduce sticky cookie dispatcher
    - Dispatch request by cookie value or query parameter value
    - Refactor sticky session dispatcher to extend the sticky cookie
    - Parse request via PCRE regexp instead of simple substring
    - Configure cookie value format pattern via PCRE syntax

---
+++ src/StickyCookie.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Copyright © Upscale Software. All rights reserved.
+ * See COPYRIGHT.txt for license details.
+ */
+namespace Upscale\Swoole\Dispatch;
+
+/**
+ * Dispatch requests to workers according to a parameter passed in cookies or in a query string.
+ * Parameter passed in a query string has higher priority than the one in cookies.
+ * Requests without the parameter context wll be delegated to a specified fallback strategy.
+ */
+class StickyCookie implements DispatchInterface
+{
+    /**
+     * @var DispatchInterface
+     */
+    protected $fallback;
+    
+    /**
+     * @var string
+     */
+    protected $cookieName;
+
+    /**
+     * @var string 
+     */
+    protected $valueFormat;
+
+    /**
+     * @var array
+     */
+    protected $dispatchMap = [];
+
+    /**
+     * Inject dependencies
+     *
+     * @param DispatchInterface $fallback Fallback strategy
+     * @param string $cookieName Cookie name (and query parameter name)
+     * @param string $valueFormat Cookie value PCRE pattern
+     */
+    public function __construct(DispatchInterface $fallback, $cookieName, $valueFormat = '[^\s;&#]+')
+    {
+        $this->fallback = $fallback;
+        $this->cookieName = preg_quote($cookieName);
+        $this->valueFormat = $valueFormat;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(\Swoole\Server $server, $fd, $type, $data)
+    {
+        if (array_key_exists($fd, $this->dispatchMap)) {
+            $workerId = $this->dispatchMap[$fd];
+        } else {
+            $requestId = $this->extractCookieValue($data);
+            if ($requestId) {
+                $workerId = $this->resolveWorkerId($server, $requestId);
+            } else {
+                $workerId = $this->fallback->__invoke($server, $fd, $type, $data);
+            }
+            $this->dispatchMap[$fd] = $workerId;
+        }
+        if ($type == self::CONNECTION_CLOSE) {
+            unset($this->dispatchMap[$fd]);
+        }
+        return $workerId;
+    }
+
+    /**
+     * Resolve given request to a worker process to serve it
+     *
+     * @param \Swoole\Server $server
+     * @param string $requestId
+     * @return int
+     */
+    protected function resolveWorkerId(\Swoole\Server $server, $requestId)
+    {
+        return abs(crc32($requestId) % $server->setting['worker_num']);
+    }
+
+    /**
+     * Extract parameter value from query string or cookies in an HTTP request message
+     *
+     * @param string $message
+     * @return string|null
+     */
+    protected function extractCookieValue($message)
+    {
+        // Exclude request body from lookup scope
+        $message = strstr($message, "\r\n\r\n", true);
+        // Query string has priority over cookies as request line precedes headers
+        if (preg_match("/\b$this->cookieName=($this->valueFormat)/", $message, $matches)) {
+            return $matches[1];
+        }  
+        return null;
+    }
+}
--- src/StickySession.php
+++ src/StickySession.php
@@ -10,33 +10,8 @@ namespace Upscale\Swoole\Dispatch;
  * Session ID is recognized in a query string and cookies in that order of priority.
  * Guest requests without the session context wll be delegated to a specified fallback strategy.
  */
-class StickySession implements DispatchInterface
+class StickySession extends StickyCookie
 {
-    /**
-     * @var DispatchInterface
-     */
-    protected $guestDispatch;
-    
-    /**
-     * @var string
-     */
-    protected $sidMarker;
-
-    /**
-     * @var int
-     */
-    protected $sidMarkerLength;
-
-    /**
-     * @var int
-     */
-    protected $sidLength;
-
-    /**
-     * @var array
-     */
-    protected $dispatchMap = [];
-
     /**
      * Inject dependencies
      *
@@ -46,61 +21,6 @@ class StickySession implements DispatchInterface
      */
     public function __construct(DispatchInterface $guestDispatch, $sessionName, $sessionIdLength)
     {
-        $this->guestDispatch = $guestDispatch;
-        $this->sidMarker = $sessionName . '=';
-        $this->sidMarkerLength = strlen($this->sidMarker);
-        $this->sidLength = $sessionIdLength;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function __invoke(\Swoole\Server $server, $fd, $type, $data)
-    {
-        if (array_key_exists($fd, $this->dispatchMap)) {
-            $workerId = $this->dispatchMap[$fd];
-        } else {
-            $sessionId = $this->extractSessionId($data);
-            if ($sessionId) {
-                $workerId = $this->resolveWorkerId($server, $sessionId);
-            } else {
-                $workerId = $this->guestDispatch->__invoke($server, $fd, $type, $data);
-            }
-            $this->dispatchMap[$fd] = $workerId;
-        }
-        if ($type == self::CONNECTION_CLOSE) {
-            unset($this->dispatchMap[$fd]);
-        }
-        return $workerId;
-    }
-
-    /**
-     * Resolve given request to a worker process to serve it
-     * 
-     * @param \Swoole\Server $server
-     * @param string $requestId
-     * @return int
-     */
-    protected function resolveWorkerId(\Swoole\Server $server, $requestId)
-    {
-        return abs(crc32($requestId) % $server->setting['worker_num']);
-    }
-
-    /**
-     * Extract session ID from an HTTP request message
-     *
-     * @param string $message
-     * @return string|null
-     */
-    protected function extractSessionId($message)
-    {
-        // Ignore request body that may contain session IDs in URLs
-        $message = strstr($message, "\r\n\r\n", true);
-        // Query string has priority over cookies as request line precedes headers  
-        $pos = strpos($message, $this->sidMarker);
-        if ($pos !== false) {
-            return substr($message, $pos + $this->sidMarkerLength, $this->sidLength);
-        }
-        return null;
+        parent::__construct($guestDispatch, $sessionName, '[0-9a-zA-Z,-]{' . $sessionIdLength . '}');
     }
 }
